df79473a38bc04b9d02312413467d42cb2d0a026
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2019-03-26 00:48:41 +0200

    +

--- src/resources/index.js
+++ src/resources/index.js
@@ -4,4 +4,8 @@ export function configure(aurelia, callback) {
   aurelia.plugin(PLATFORM.moduleName('resources/elements/aurelia-content-loader/src'), callback);
   // aurelia.plugin(PLATFORM.moduleName('resources/elements/aurelia-content-loader/dist/es2015'), callback);
   // aurelia.plugin(PLATFORM.moduleName('aurelia-content-loader'), callback);
+
+  aurelia.plugin(PLATFORM.moduleName('resources/elements/aurelia-google-recaptcha/src'), callback);
+  // aurelia.plugin(PLATFORM.moduleName('resources/elements/aurelia-google-recaptcha/dist/es2015'), callback);
+  // aurelia.plugin(PLATFORM.moduleName('aurelia-google-recaptcha'), callback);
 }
